f97a45b1758c78befd19367d3c103cec717e309d
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-07-04 16:09:17 +0200

    customers give score per order size

---
+++ HellOfAManagement/Assets/Game/Customers/CustomerData/Scripts/CustomerScoreData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CustomerScoreData", menuName = "ScriptableObjects/CustomerScoreData", order = 8)]
+public class CustomerScoreData : ScriptableObject
+{
+    public float[] scoresGiven;
+}
