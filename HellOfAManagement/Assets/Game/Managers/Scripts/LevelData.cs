08551de98cff88a87afd49dc1e63de2176c1bbf4
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-07-04 15:50:32 +0200

    Push script change

--- HellOfAManagement/Assets/Game/Managers/Scripts/LevelData.cs
+++ HellOfAManagement/Assets/Game/Managers/Scripts/LevelData.cs
@@ -1,24 +1,22 @@
 ﻿using UnityEngine;
 
-namespace HOM
+
+[CreateAssetMenu(fileName = "New Levels Data", menuName = "ScriptableObjects/Levels Data", order = 6)]
+public sealed class LevelData : ScriptableObject
 {
-    [CreateAssetMenu(fileName = "New Levels Data", menuName = "ScriptableObjects/Levels Data", order = 6)]
-    public sealed class LevelData : ScriptableObject
+    [System.Serializable]
+    public struct Level
     {
-        [System.Serializable]
-        public struct Level
-        {
-            public string levelName;
-            public string levelGoalDescription;
-            public string LevelLogoPath;
+        public string levelName;
+        public string levelGoalDescription;
+        public string LevelLogoPath;
 
-            public uint firstStarScore;
-            public uint secondStarScore;
-            public uint thirdStarScore;
-        }
-
-        [SerializeField] Level[] levels;
-
-        public Level GetLevel(uint id) => levels[id];
+        public uint firstStarScore;
+        public uint secondStarScore;
+        public uint thirdStarScore;
     }
+
+    [SerializeField] Level[] levels;
+
+    public Level GetLevel(uint id) => levels[id];
 }
