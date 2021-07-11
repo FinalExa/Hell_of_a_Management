1a0fcdecd39a878d2a505a1b5d60233003cd29ac
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-07-11 15:13:00 +0200

    Unified mouse scripts

--- Assets/Game/Mouse/Scripts/ObjectsOnMouse.cs
+++ Assets/Game/Mouse/Scripts/ObjectsOnMouse.cs
@@ -1,7 +1,30 @@
 ﻿using UnityEngine;
 
-public class ObjectsOnMouse : MouseData
+public class ObjectsOnMouse : MonoBehaviour
 {
+    public RaycastHit hit;
+    public Ray ray;
+    [HideInInspector] public Vector3 mousePositionInSpace;
+    private Camera mainCamera;
+
+    public void Awake()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+    }
+    public void FixedUpdate()
+    {
+        MouseRaycast();
+    }
+    public void MouseRaycast()
+    {
+        ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out hit);
+        mousePositionInSpace = hit.point;
+    }
+    public RaycastHit GetMousePosition()
+    {
+        return hit;
+    }
     public bool CheckForThrowableObject(Collider hit)
     {
         if (hit != null && hit.GetComponent<IThrowable>() != null)
