c8522ef844e572203cb84610306f08c567d8b585
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-07-17 17:28:12 +0200

    Stopped all sounds when going to main menu

--- Assets/Game/UI/Scripts/EndScreen.cs
+++ Assets/Game/UI/Scripts/EndScreen.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 public class EndScreen : MonoBehaviour
 {
     Animator animator;
@@ -17,6 +15,7 @@ public class EndScreen : MonoBehaviour
 
     public void GoToMainMenu()
     {
+        AudioManager.instance.StopAllSounds();
         animator.SetTrigger("EndGame");
         LevelManager.LoadLevel("Main Menu");
     }
