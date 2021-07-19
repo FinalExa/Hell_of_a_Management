1d5e2e6911cc6a230c39ef99cfd2ba35dbb9add6
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-07-19 11:30:44 +0200

    Mop implemented

---
+++ Assets/Modules/Gameplay/Surfaces Management/Mop/Mop.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Mop : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Terrain"))
+        {
+            SurfaceController sc = other.gameObject.GetComponent<SurfaceController>();
+            if (sc != null) SurfaceManager.DeactivateSurface(ref sc);
+        }
+    }
+}
